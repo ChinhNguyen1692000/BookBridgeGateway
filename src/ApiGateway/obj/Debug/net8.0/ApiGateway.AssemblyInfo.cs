8ef85af7a88c84b6f1d61db4a0a7265d7f0efbbd
nlhchinh <chinhnguyen1692000z@gmail.com>
2025-10-19 15:40:02 +0700

    Push

--- src/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ src/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a55c5768c658e6b174878df833d07f044f96cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea2e729c67f60d2ca332c69c856843bd24b02231")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
